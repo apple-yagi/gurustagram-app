8c307430d9a68367d05dc3d42647725cbda45b7c
apple-yagi <applesryu@gmail.com>
2020-03-06 22:26:14 +0900

    create GstButton component

---
+++ src/components/atoms/GstButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn :class="classes" :disabled="disabled" @click="handleClick">
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "GstButton",
+
+  props: {
+    classes: {
+      type: String,
+      default: "btn"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    handleClick(ev) {
+      this.$emit("click", ev);
+    }
+  }
+};
+</script>
