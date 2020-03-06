8fed3755a67b6c7459a7f223f9738b9d68d0ea09
apple-yagi <applesryu@gmail.com>
2020-03-06 22:25:20 +0900

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
+import 'es6-promise/auto'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase/app'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+}
+
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
