5b3e52059de20b1aeb2a8d4da9a670c25fd43ecd
apple-yagi <applesryu@gmail.com>
2020-03-06 22:26:44 +0900

    create GstLoginView

---
+++ src/views/GstLoginView.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-layout justify-center>
+      <GstLoginForm :onlogin="handleLogin" />
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import GstLoginForm from "@/components/molecules/GstLoginForm";
+
+export default {
+  name: "GstLoginView",
+
+  components: {
+    GstLoginForm
+  },
+
+  methods: {
+    handleLogin(authInfo) {
+      return this.$store
+        .dispatch("login", authInfo)
+        .then(() => {
+          this.$router.push({ path: "/" });
+        })
+        .catch(err => {
+          this.throwReject(err);
+        });
+    },
+    throwReject(err) {
+      return Promise.reject(err);
+    }
+  }
+};
+</script>
