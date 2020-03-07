78cc7f321ec5552a1948376c93dd077f443686c4
apple-yagi <applesryu@gmail.com>
2020-03-07 23:06:11 +0900

    update

--- src/api/logined.js
+++ src/api/logined.js
@@ -3,6 +3,14 @@ import 'firebase/auth'
 
 export default {
   getCurrentUser() {
-    return firebase.auth().currentUser
+    return new Promise((resolve, reject) => {
+      const user = firebase.auth().currentUser
+      if (user) {
+        resolve(user)
+      }
+      else {
+        reject("userが取得できませんでした")
+      }
+    })
   }
 }
