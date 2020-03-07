e46b612cc1c8d9f928ca0ccbec6d738c79d101ea
apple-yagi <applesryu@gmail.com>
2020-03-07 16:07:29 +0900

    create GstAlertMessage

---
+++ src/components/atoms/GstAlertMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-alert :type="alertType">{{ message }}</v-alert>
+</template>
+
+<script>
+export default {
+  name: "GstAlertMessage",
+
+  props: {
+    alertType: {
+      type: String,
+      default: "info"
+    },
+    message: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
