6d5b43115c44d31ccbeea3049897f9120ee1e170
apple-yagi <applesryu@gmail.com>
2020-03-07 23:06:50 +0900

    create Footer, Header components

---
+++ src/components/molecules/GstFooter.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-footer app color="secondary" dark>
+    <v-spacer></v-spacer>
+    <div>Copyright ©︎ {{ new Date().getFullYear() }} YAGI</div>
+  </v-footer>
+</template>
---
+++ src/components/molecules/GstHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app color="secondary" dark>
+    <v-btn to="/">
+      <v-icon>zmdi zmdi-cutlery</v-icon>
+      <span class="shrink ml-2 hidden-sm-and-down">Gurustagram</span>
+    </v-btn>
+
+    <v-spacer />
+    <v-btn class="mr-2" to="/post">
+      <v-icon>mdi-send</v-icon>
+      <span class="shrink ml-2 hidden-sm-and-down">Post</span>
+    </v-btn>
+    <v-btn to="/account">
+      <v-icon>zmdi zmdi-account-circle</v-icon>
+      <span class="shrink ml-2 hidden-sm-and-down">Account</span>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
