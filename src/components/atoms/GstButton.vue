45db496dec889bbaf8716fca4e57f1220a00d752
apple-yagi <applesryu@gmail.com>
2020-03-08 18:39:20 +0900

    update

--- src/components/atoms/GstButton.vue
+++ src/components/atoms/GstButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn :class="classes" :disabled="disabled" @click="handleClick">
+  <v-btn :tile="tile" :class="classes" :height="height" :disabled="disabled" @click="handleClick">
     <slot />
   </v-btn>
 </template>
@@ -14,6 +14,14 @@ export default {
       default: "btn"
     },
     disabled: {
+      type: Boolean,
+      default: false
+    },
+    height: {
+      type: Number,
+      default: 40
+    },
+    tile: {
       type: Boolean,
       default: false
     }
