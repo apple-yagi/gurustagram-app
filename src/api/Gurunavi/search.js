fd66179d46ce4a26deba64b6ed96659296422563
apple-yagi <applesryu@gmail.com>
2020-03-22 19:02:52 +0900

    エラーハンドリング を追加

--- src/api/Gurunavi/search.js
+++ src/api/Gurunavi/search.js
@@ -12,8 +12,38 @@ export default {
         .then(shops => {
           resolve(shops.data.rest)
         })
-        .catch(err => {
-          reject("お店が見つかりませんでした")
+        .catch(error => {
+          const errorStatus = error.response.status;
+
+          switch (errorStatus) {
+            case 400:
+              reject("不正なパラメータが指定されました");
+              break;
+
+            case 401:
+              reject("不正なアクセスです");
+              break;
+
+            case 404:
+              reject("お店が見つかりませんでした");
+              break;
+
+            case 405:
+              reject("不正なアクセスです");
+              break;
+
+            case 429:
+              reject("リクエスト回数上限超過");
+              break;
+
+            case 500:
+              reject("処理中にエラーが発生しました");
+              break;
+
+            default:
+              reject("不明なエラーです");
+              break;
+          }
         });
     })
   }
