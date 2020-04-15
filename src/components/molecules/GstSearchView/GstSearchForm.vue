1a1aa325cb5e41fd8a1bf5385bdbc5a17768508c
apple-yagi <applesryu@gmail.com>
2020-04-16 00:10:58 +0900

    都道府県検索結果の表示

--- src/components/molecules/GstSearchView/GstSearchForm.vue
+++ src/components/molecules/GstSearchView/GstSearchForm.vue
@@ -1,27 +1,28 @@
 <template>
   <v-container fluid>
-    <v-row align="center">
+    <v-row justify="center">
       <v-col cols="12" sm="8" md="8" lg="6">
-        <v-layout justify-space-between>
-          <v-select v-model="selectedArea" :items="areaList" attach chips label="エリア" />
-          <GstButton
-            :tile="custom_GstButton.tile"
-            :height="custom_GstButton.height"
-            :loading="custom_GstButton.loading"
-            @click="searchShops()"
-          >
-            <v-icon>mdi-magnify</v-icon>
-            <span class="shrink ml-2 hidden-sm-and-down">検索</span>
-          </GstButton>
-        </v-layout>
+        <v-form>
+          <v-layout justify-space-between>
+            <v-select v-model="selectedArea" :items="areaList" attach chips label="エリア" />
+            <GstButton
+              :tile="custom_GstButton.tile"
+              :height="custom_GstButton.height"
+              :loading="custom_GstButton.loading"
+              @click="searchShops()"
+            >
+              <v-icon>mdi-magnify</v-icon>
+              <span class="shrink ml-2 hidden-sm-and-down">検索</span>
+            </GstButton>
+          </v-layout>
+          <GstAlertMessage
+            v-show="notPostedShops"
+            :alert-type="custom_GstAlertMessage.alertType"
+            :message="custom_GstAlertMessage.error_msg"
+          />
+        </v-form>
       </v-col>
-      <GstAlertMessage
-        v-show="notPostedShops"
-        :alert-type="custom_GstAlertMessage.alertType"
-        :message="custom_GstAlertMessage.error_msg"
-      />
     </v-row>
-    <div v-for="(shop, index) in postedShops" :key="index">{{ shop.name }}</div>
   </v-container>
 </template>
 
@@ -34,7 +35,6 @@ import { Shops } from "@/api/index";
 
 const custom_GstButton = {
   height: 50,
-  tile: true,
   loading: false
 };
 
@@ -77,6 +77,9 @@ export default {
       this.postedShops = await Shops.loadPostedShopsByAreaCode(
         selectedAreaCode
       );
+
+      // 親コンポーネントに伝搬
+      this.$emit("refPostedShops", this.postedShops);
 
       this.custom_GstButton.loading = false;
     }
--- src/views/GstSearchView.vue
+++ src/views/GstSearchView.vue
@@ -1,19 +1,75 @@
 <template>
   <v-container>
     <v-layout justify-center>
-      <GstSearchForm />
+      <GstSearchForm @refPostedShops="refPostedShops($event)" />
     </v-layout>
+    <div class="responsive-img-size" style="margin: auto;">
+      <div v-for="(shop, index) in postedShops" :key="index">
+        <GstPostedCard :Shop="shop" v-on:openDialog="openDialog(shop)" />
+        <GstPostedDialog
+          v-if="currentShop == shop"
+          :current-shop="currentShop"
+          :dialog="dialog"
+          v-on:closeDialog="closeDialog()"
+        />
+      </div>
+    </div>
   </v-container>
 </template>
 
+<style scoped>
+@media screen and (min-width: 767px) {
+  .responsive-img-size {
+    width: 550px;
+    height: 450px;
+  }
+  .responsive-font-size {
+    font-size: 1.25rem;
+  }
+}
+
+hr {
+  border-top: 1px dotted #bbb;
+  border-bottom: 1px dotted #fff;
+}
+</style>
+
 <script>
 import GstSearchForm from "@/components/molecules/GstSearchView/GstSearchForm";
+import GstPostedCard from "@/components/molecules/GstPostedCard";
+import GstPostedDialog from "@/components/molecules/GstPostedDialog";
 
 export default {
   name: "GstSearchView",
 
   components: {
-    GstSearchForm
+    GstSearchForm,
+    GstPostedCard,
+    GstPostedDialog
+  },
+
+  data() {
+    return {
+      postedShops: [],
+      currentShop: null,
+      dialog: false
+    };
+  },
+
+  methods: {
+    refPostedShops(eventVal) {
+      this.postedShops = eventVal;
+    },
+
+    openDialog: function(shop) {
+      this.currentShop = shop;
+      this.dialog = true;
+    },
+
+    closeDialog() {
+      this.currentShop = null;
+      this.dialog = false;
+    }
   }
 };
 </script>
